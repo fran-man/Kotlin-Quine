1a78d7c5b814ab3863dab2c5cba49efb082b6c57
fran-man <fran.man@hotmail.co.uk>
2020-08-29 13:40:35 +0100

    Now I really have done it!

--- src/main/kotlin/com/franm/quine/FileToByteArrays.kt
+++ src/main/kotlin/com/franm/quine/FileToByteArrays.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 fun main() {
     var lineNo = 1
-    File("C:\\Users\\franm\\dev\\Kotlin-Quine\\src\\main\\kotlin\\com\\franm\\quine\\Quine.kt").forEachLine {
+    File("C:\\Users\\franm\\dev\\Kotlin-Quine\\src\\main\\kotlin\\com\\franm\\quine\\QuineSrc.txt").forEachLine {
         print("listOf(")
         it.toByteArray().forEach {
             print(it)
